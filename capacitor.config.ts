d0c2d82425998eb27e6935a70df3bdda0e224262
Ippei Nagamine <1pei.nag@gmail.com>
2026-01-03 12:37:19 +0900

    Rename app to らくがきパレット

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.chalkboard.physics',
-  appName: 'chalkboard-physics',
+  appName: 'らくがきパレット',
   webDir: 'out'
 };
 
